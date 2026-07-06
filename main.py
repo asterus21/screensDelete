ed83c5f5aa794d54dbd18be0c1c7883c9d11fb19
Michael Paul <michaelpaulwem@gmail.com>
2026-07-06 10:21:36 +0300

    script updated

--- main.py
+++ main.py
@@ -6,47 +6,49 @@ These screenshots are not used anymor in the PolyAnalyst User's Manual.
 import csv
 import os
 import subprocess
+import sys
 
 
 def start_script(input_path: str, output_path: str) -> None:
     """Starts the script to create two files with missed and unused screenshots."""
     subprocess.run(
         f"unused-images.exe --help-path {input_path} --output-path {output_path}",
-            shell=True,
-                check=False
+        shell=True,
+        check=False
     )
 
 
 def read_file(file: str) -> list:
     """Opens the file and makes a Python list."""
     with open(file, "r", newline="", encoding="UTF-8") as f:
-        csv_reader = csv.reader(f)
-        data_list = list(csv_reader)
+        data_list = list(csv.reader(f))
     return data_list
 
 
-def fetch_items(l: list, p: str) -> list:
+def fetch_items(files: list, path: str) -> list:
     """Creates a valid path for the images."""
-    x = [f"{p}" + "/images/" + sublist[0] for sublist in l]
+    x = [f"{path}" + "/images/" + sublist[0] for sublist in files]
     return x
 
 
 def delete_files(l: list) -> None:
     """Deletes files by an accepted path."""
     for file in l:
-        if os.path.exists(file):
+        try:
+            os.path.exists(file)
             os.remove(file)
             print(f"File {file} deleted successfully.")
-        else:
+        except:
             print(f"File {file} does not exist.")
+            sys.exit(0)
 
 
 def main() -> None:
     """Main function."""
-    input_path = input("Enter the input path (e.g. D:/gitbash/help): ").strip()
+    input_path  = input("Enter the input path (e.g. D:/gitbash/help): ").strip()
     output_path = input("Enter the output path (e.g. ./): ").strip()
-    # starting the external script
     start_script(input_path, output_path)
+    # uncomment to delete files automatically
     # delete_files(fetch_items(read_file("unused.csv"), input_path))
 
 
